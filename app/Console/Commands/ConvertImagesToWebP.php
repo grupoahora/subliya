5fd3410a91aced3504f3a8f762c58580db85c767
Andres Patiño <82970167+grupoahora@users.noreply.github.com>
2023-07-13 19:16:54 -0500

    change format webp

---
+++ app/Console/Commands/ConvertImagesToWebP.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\File;
+
+class ConvertImagesToWebP extends Command
+{
+    protected $signature = 'images:convert';
+
+    protected $description = 'Convert images to WebP format';
+
+    public function handle()
+    {
+        $directory = public_path('image');
+        $files = File::files($directory);
+
+        foreach ($files as $file) {
+            $image = Image::make($file);
+            $image->encode('webp');
+            $image->save($directory . '/' . pathinfo($file, PATHINFO_FILENAME) . '.webp');
+        }
+
+        $this->info('Images converted successfully!');
+    }
+}
